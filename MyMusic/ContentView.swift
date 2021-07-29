186c2379133de00782bf05644b0a280532ec7c0c
chiakiaosima <starsrock5937@gmail.com>
2021-07-30 07:47:03 +0900

    Initial Commit

---
+++ MyMusic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyMusic
+//
+//  Created by 青島智明 on 2021/07/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyMusic/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  MyMusic
+//
+//  Created by 青島智明 on 2021/07/30.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
