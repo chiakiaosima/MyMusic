e7c0b8dd9f2f4dad2676b658dbf0aea46ed4fb16
chiakiaosima <starsrock5937@gmail.com>
2021-07-31 07:17:58 +0900

    背景画像とボタンの配置

--- MyMusic/ContentView.swift
+++ MyMusic/ContentView.swift
@@ -9,10 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+        ZStack {
+            // 背景画像を指定する
+            Image("background")
+            // リサイズする
+                .resizable()
+            // 画面いっぱいになる様にセーフエリア外までいっぱいになる様に指定
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            // アスペクト比（縦横比）を維持して短辺基準に収まる様にする
+                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+            
+            // 水平にレイアウト（横方向にレイアウト）
+            HStack {
+                //　シンバルボタン
+                Button(action: {
+                    // ボタンをタップした時のアクション
+                }) {
+                    //　画像を表示する
+                    Image("cymbal")
+                    // ボタン内の画像をカラー画像となる様に指定する
+                        .renderingMode(.original)
+                } //シンバルボタンここまで
+                
+                // ギターボタン
+                Button(action: {
+                    //　ボタンをタップした時のアクション
+                }) {
+                    //　画像を表示する
+                    Image("guitar")
+                    //　ボタン内の画像をカラー画像となる様に指定する
+                        .renderingMode(.original)
+                } //ギターボタン　ここまで
+            } //Hstack　ここまで
+        }  // ZStack　ここまで
+    }  // body　ここまで
+
+}  // ContentViewここまで
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
